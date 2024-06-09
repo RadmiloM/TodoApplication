3871b901ebe40414d0fe5192d14c53a7f14a62cd
RadmiloM <markovicradmilo35@gmail.com>
2024-06-09 09:40:39 +0200

    added displayed user item and user item class

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const textElement = document.querySelector('.text-field')
 const submitButton = document.querySelector('.submit-button');
 submitButton.addEventListener('click',printInputValue);
+const userItem = document.querySelector('.user-item');
 
 function printInputValue() {
-    console.log(textElement.value);
+    const textValue = textElement.value;
+    userItem.innerHTML = textValue;
 }
